76047ed10625cb15277317acccbdce464354d921
DEVULAA <122668031+DEVULAA@users.noreply.github.com>
2023-01-14 09:52:21 +0100

    Update tost pronote.py

--- tost pronote.py
+++ tost pronote.py
@@ -1,28 +1,35 @@
 import pronotepy
 import datetime
-from sys import exit
-from pronotepy.ent import l_normandie
+from flask import Flask, jsonify
+
+app = Flask(__name__)
 
 tps=0
 
 client = pronotepy.Client('https://0760095r.index-education.net/pronote/eleve.html',
                       username='u.devalland1',
                       password='Boubou2701!',
-                      ent=l_normandie) # ent specific
+                      ent=l_normandie)
 
 def custom_command():
     if client.logged_in:
-        nom_utilisateur = client.info.name # get users name
-        print(f'Bienvenue {nom_utilisateur}!')
+        nom_utilisateur = client.info.name
         periods = client.periods
 
-        today = datetime.date.today() # store today's date using datetime built-in library
-        homework = client.homework(today) # get list of homework for today and later
+        today = datetime.date.today()
+        homework = client.homework(today) 
         homeworkv2 = homework[:4]
-        homework_list = [] # create an empty list to store the homework
-        for hw in homeworkv2: # iterate through the list
-            homework_list.append(f"({hw.date})({hw.subject.name}): {hw.description}") # add the homework's subject, title and description to the list
-        return homework_list # return the list of homework
+        homework_list = []
+        for hw in homeworkv2:
+            homework_list.append(f"({hw.date})({hw.subject.name}): {hw.description}")
+        return homework_list
     else: 
         return("Failed to log in")
-        exit()
+
+@app.route('/homework')
+def get_homework():
+    homework_list = custom_command()
+    return jsonify(homework_list)
+    
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
